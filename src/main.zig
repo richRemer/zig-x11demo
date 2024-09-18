fcc673cc02f6fae8602a52d39cb31b34b18a6fbf
Richard Remer <rich.remer@gmail.com>
2024-09-18 08:48:44 -0700

    detect X11 display name

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const x11 = @import("x11.zig");
 
-pub fn main() void {
+pub fn main() !void {
   std.log.info("X11 version {}.", .{x11.protocolVersion()});
-  //std.log.info("X11 Display {}.", .{x11.openDisplay()});
+  std.log.info("X11 Display {}.", .{try x11.openDisplay(null)});
 }
--- src/x11.zig
+++ src/x11.zig
@@ -3,12 +3,20 @@ const c = @cImport({
   @cInclude("X11/X.h");
 });
 
-pub export fn protocolVersion() u8 {
+pub fn protocolVersion() u8 {
   return c.X_PROTOCOL;
 }
 
-pub export fn openDisplay() u8 {
+pub fn openDisplay(display: ?[:0] const u8) !u8 {
   log.debug("opening X11 display", .{});
+
+  const display_name = display orelse std.posix.getenv("DISPLAY");
+
+  if (display_name == null) {
+    log.err("no DISPLAY environment variable", .{});
+    return error.X11NoDisplay;
+  }
+
   return c.X_PROTOCOL;
 }
 
