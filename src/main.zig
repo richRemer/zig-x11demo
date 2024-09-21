987c8fb62879163329e54b9fd22aa6a7e608b85e
Richard Remer <rich.remer@gmail.com>
2024-09-21 01:55:46 -0700

    parse X11 display name

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const x11 = @import("x11.zig");
 
 pub fn main() !void {
-  std.log.info("X11 version {}.", .{x11.protocolVersion()});
-  std.log.info("X11 Display {}.", .{try x11.openDisplay(null)});
+  std.log.info("X11 version {}", .{x11.protocolVersion()});
+
+  const display = try x11.Display.init(null);
+
+  std.log.info("X11 Display {s}", .{display.name});
 }
