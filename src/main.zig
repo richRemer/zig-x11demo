45d243429ff73bc1c87a60454d6f08b111ca934b
Richard Remer <rich.remer@gmail.com>
2024-10-05 00:15:10 -0700

    replace some magic numbers

--- src/main.zig
+++ src/main.zig
@@ -28,26 +28,27 @@ fn handleMessage(message: x11.Message) void {
 }
 
 fn eventLoop(server: *x11.Server) u8 {
+    const linux = std.os.linux;
     const fd_count = 1;
     const unlimited_timeout: i32 = -1;
 
     var running = true;
-    var pollfds: [1]std.os.linux.pollfd = undefined;
+    var pollfds: [1]linux.pollfd = undefined;
 
-    pollfds[0] = std.os.linux.pollfd{
+    pollfds[0] = linux.pollfd{
         .fd = server.connection.stream.handle,
-        .events = 0x1, // POLLIN (from poll.h)
+        .events = linux.POLL.IN,
         .revents = 0,
     };
 
     while (running) {
-        _ = std.os.linux.poll(&pollfds, fd_count, unlimited_timeout);
+        _ = linux.poll(&pollfds, fd_count, unlimited_timeout);
 
-        if (pollfds[0].revents & 0x8 == 0x8) { // POLERR (from poll.h)
+        if (pollfds[0].revents & linux.POLL.ERR == linux.POLL.ERR) {
             x11.log.err("poll error", .{});
         }
 
-        if (pollfds[0].revents & 0x10 == 0x10) { // POLLHUP (from poll.h)
+        if (pollfds[0].revents & linux.POLL.HUP == linux.POLL.HUP) {
             x11.log.debug("connection closed", .{});
             running = false;
         }
